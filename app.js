a62e7c87b31a9c2f2b92f8c53e8b3b58d69f3f56
Dominik <ledom.git@gmail.com>
2016-09-21 16:42:36 +0200

    Use Node functions inside Express

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-	response.send('Hello world!');
+	response.write('Hello world!');
+	response.end();
 });
 
 app.listen(3000, function() {
