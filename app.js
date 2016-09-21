e68c179571c9ef5e87c7f8bb2ad149b46a43ad69
Dominik <ledom.git@gmail.com>
2016-09-21 16:47:00 +0200

    The send function converts Objects and Arrays to JSON

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
 	response.end();
 });
 
+app.get('/blocks', function(request, response) {
+	var blocks = ['Fixed', 'Movable', 'Rotating'];
+	response.send(blocks);
+});
+
 app.listen(3000, function() {
 	console.log('Listening on port 3000');
 });
